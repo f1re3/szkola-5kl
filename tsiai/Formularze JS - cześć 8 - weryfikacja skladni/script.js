014336ce4c57753d60536f043f70591d36afb033
Daniel Sobiech <sobiech151205@gmail.com>
2024-09-24 12:40:29 +0200

    24.09

--- tsiai/Formularze JS - cześć 8 - weryfikacja skladni/script.js
+++ tsiai/Formularze JS - cześć 8 - weryfikacja skladni/script.js
@@ -8,8 +8,17 @@ BtnS.addEventListener('click', (e)=>{
     let firstnE_V = firstnE.value
     let surnE_V = surnE.value
     let ageE_V = Number(ageE.value)
-    if(firstnE_V.length<3 || surnE_V.length<3 || ageE_V<0 && ageE_V>120){
-        
+    if(ageE_V<0 && ageE_V>120){
+        ageE.focus()
+        ageE.placeholder = 'wiek od 0 do 120'
+    }
+    else if(firstnE_V.length<3){
+        firstnE.focus()
+        firstnE.placeholder = 'imie powinno miec cnm 3 znaki'
+    }
+    else if(surnE_V.length<3){
+        surnE.focus()
+        surnE.placeholder = 'nazwisko powinno miec cnm 3 znaki'
     }
     else
         alert(`witaj ${firstnE_V} ${surnE_V}`)
