a2ae797fd4751187032a765aa70f56521196fbca
Daniel Sobiech <sobiech151205@gmail.com>
2024-09-26 13:10:32 +0200

    26.09

--- tsiai/Formularze JS - cześć 8 - weryfikacja skladni/script.js
+++ tsiai/Formularze JS - cześć 8 - weryfikacja skladni/script.js
@@ -8,18 +8,36 @@ BtnS.addEventListener('click', (e)=>{
     let firstnE_V = firstnE.value
     let surnE_V = surnE.value
     let ageE_V = Number(ageE.value)
-    if(ageE_V<0 && ageE_V>120){
+    if(ageE_V<0 || ageE_V>120){
         ageE.focus()
+        ageE.value = ''
         ageE.placeholder = 'wiek od 0 do 120'
-    }
-    else if(firstnE_V.length<3){
-        firstnE.focus()
-        firstnE.placeholder = 'imie powinno miec cnm 3 znaki'
-    }
-    else if(surnE_V.length<3){
-        surnE.focus()
-        surnE.placeholder = 'nazwisko powinno miec cnm 3 znaki'
+        console.log(ageE_V)
     }
     else
+        ageE.style.backgroundColor = ''    
+
+    if(firstnE_V.length<3){
+        firstnE.focus()
+        firstnE.value = ''
+        firstnE.placeholder = 'imie powinno miec cnm 3 znaki'
+        console.log(firstnE_V)
+    }
+    else
+        firstnE.style.backgroundColor = ''  
+
+    if(surnE_V.length<3){
+        surnE.focus()
+        surnE.value = ''
+        surnE.placeholder = 'nazwisko powinno miec cnm 3 znaki'
+        console.log(surnE_V)
+    }
+    else
+        surnE.style.backgroundColor = ''  
+
+    if(ageE_V>0 && ageE_V<120 && firstnE_V.length>3 && surnE_V.length>3){
         alert(`witaj ${firstnE_V} ${surnE_V}`)
+    }
+
+    
 })
